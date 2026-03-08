7a49283a80b23b7585744d7f1f1e0ce69a854c49
Iliya Karin <iliyakarin.it@gmail.com>
2026-03-08 15:58:33 -0500

    Mock init implementation

---
+++ mock-fed-gateway/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String, Integer, Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class Bank(Base):
+    __tablename__ = "banks"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    routing_number: Mapped[str] = mapped_column(String(9), unique=True, nullable=False, index=True)
+
+class Merchant(Base):
+    __tablename__ = "merchants"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    merchant_id: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
+    category: Mapped[str] = mapped_column(String(100), nullable=False)
