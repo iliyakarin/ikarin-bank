d5fdf802cc0649beb7257cf8b5a817487f72c8a7
Iliya Karin <iliyakarin.it@gmail.com>
2026-03-07 21:22:07 -0600

    Clickhouse fix

--- init-db/clickhouse_init.sql
+++ init-db/clickhouse_init.sql
@@ -2,14 +2,21 @@ CREATE DATABASE IF NOT EXISTS banking_log;
 
 CREATE TABLE IF NOT EXISTS banking_log.transactions (
     transaction_id String,
-    sender_id Int64,
-    recipient_email String,
+    parent_id Nullable(String),
+    account_id Int64,
+    sender_email Nullable(String),
+    recipient_email Nullable(String),
     amount Float64,
-    timestamp DateTime,
+    category String,
+    merchant String,
+    transaction_type String,
+    transaction_side String,
+    event_time DateTime,
+    internal_account_last_4 Nullable(String),
     status String
 ) ENGINE = ReplacingMergeTree()
-PARTITION BY toYYYYMM(timestamp)
-ORDER BY (timestamp, transaction_id);
+PARTITION BY toYYYYMM(event_time)
+ORDER BY (account_id, event_time, transaction_id);
 
 CREATE TABLE IF NOT EXISTS banking_log.activity_events (
     event_id        String,
